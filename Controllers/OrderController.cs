a3cb47266e3a2104c9d95841c020a786196a0b58
JoaoLimaBrr <joaovitorsoareslima12@gmail.com>
2023-04-03 21:16:08 -0300

    remoção de calculos da controller

--- Controllers/OrderController.cs
+++ Controllers/OrderController.cs
@@ -13,61 +13,28 @@ public class OrderController : ControllerBase
 {
     private readonly ICustumerRepository _custumerRepository;
     private readonly IDeliveryFeeService _deliveryFeeService;
+    private readonly IPromoCodeRepository _promoCodeRepository;
 
-    public OrderController(ICustumerRepository custumerRepository, IDeliveryFeeService deliveryFeeService)
+    public OrderController(ICustumerRepository custumerRepository, IDeliveryFeeService deliveryFeeService, IPromoCodeRepository promoCodeRepository)
     {
         _custumerRepository = custumerRepository;
         _deliveryFeeService = deliveryFeeService;
+        _promoCodeRepository = promoCodeRepository;
     }
 
     [Route("v1/orders")]
     [HttpPost]
-    public async Task<IActionResult> Place(string customerId, string zipCode, string promoCode, int[] products)
+    public async Task<string> Place(string customerId, string zipCode, string promoCode, int[] products)
     {
-        // #1 - Recupera o cliente
         var custumer = await _custumerRepository.GetById(customerId);
 
-        // #2 - Calcula o frete
         var deliveryFee = await _deliveryFeeService.GetDeliveryFeeAsync(zipCode);
 
-        // #3 - Calcula o total dos produtos
-        decimal subTotal = 0;
-        const string getProductQuery = "SELECT [Id], [Name], [Price] FROM PRODUCT WHERE ID=@id";
-        for (var p = 0; p < products.Length; p++)
-        {
-            Product product;
-            await using (var conn = new SqlConnection("CONN_STRING"))
-                product = await conn.QueryFirstAsync<Product>(getProductQuery, new { Id = p });
+        var cupom = await _promoCodeRepository.GetPromoCodeAsync(promoCode);
 
-            subTotal += product.Price;
-        }
+        var discount = cupom?.Value > 0 ? cupom.Value : 0;
 
-        // #4 - Aplica o cupom de desconto
-        decimal discount = 0;
-        await using (var conn = new SqlConnection("CONN_STRING"))
-        {
-            const string query = "SELECT * FROM PROMO_CODES WHERE CODE=@code";
-            var promo = await conn.QueryFirstAsync<PromoCode>(query, new { code = promoCode });
-            if (promo.ExpireDate > DateTime.Now)
-                discount = promo.Value;
-        }
-
-        // #5 - Gera o pedido
-        var order = new Order();
-        order.Code = Guid.NewGuid().ToString().ToUpper().Substring(0, 8);
-        order.Date = DateTime.Now;
-        order.DeliveryFee = deliveryFee;
-        order.Discount = discount;
-        order.Products = products;
-        order.SubTotal = subTotal;
-
-        // #6 - Calcula o total
-        order.Total = subTotal - discount + deliveryFee;
-
-        // #7 - Retorna
-        return Ok(new
-        {
-            Message = $"Pedido {order.Code} gerado com sucesso!"
-        });
+        var order = new Order(deliveryFee, discount, new List<Product>());
+        return $"Pedido {order.Code} gerado com sucesso!";
     }
 }
